e6a13433e883269a1602f9b08e000999cf0bd756
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-19 23:20:44 -0400

    Actualizar banner.py

--- banner.py
+++ banner.py
@@ -46,7 +46,7 @@ table_data = [
 
 ]
 
-print(Fore.RED + Back.BLACK + tabulate(table_data + tablefmt="fancy_grid") + Style.RESET_ALL)
+print(Fore.RED + Back.BLACK + tabulate(table_data, tablefmt="fancy_grid") + Style.RESET_ALL)
 print(Style.BRIGHT + Fore.YELLOW + "[" + Fore.GREEN + "+" + Fore.YELLOW + "]" + Fore.WHITE + "Stellar [V1.0]" + Style.RESET_ALL)
 print(Style.BRIGHT + Fore.YELLOW + "[" + Fore.GREEN + "+" + Fore.YELLOW + "]", Fore.WHITE + "Para ver comandos utilice: menu", Style.RESET_ALL)
 print(Style.BRIGHT + Fore.YELLOW + "[" + Fore.GREEN + "+" + Fore.YELLOW + "]", Fore.WHITE + "Hora actual: " + hour_string + " │ Fecha: " + date_string + Style.RESET_ALL)
