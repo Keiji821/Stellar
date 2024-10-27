7146c8d4657448d36099ab883d61cf4463f1600a
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-27 14:57:14 -0400

    Actualizar update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-# Verificar si el archivo.bashrc existe en la carpeta stellar
-if [! -f ~/Stellar/.bashrc ]; then
-  # Crear un nuevo archivo.bashrc en la carpeta stellar
-  touch ~/Stellar/.bashrc
-  echo "Archivo.bashrc creado de nuevo en la carpeta stellar!"
-fi
