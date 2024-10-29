e533e5b05862e2086545fb35f57b97b2faf263cd
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-29 19:56:15 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -4,14 +4,28 @@ from tabulate import tabulate
 
 init()
 
-menu_data = [
-    ["reload", "RECARGAR BANNER"],
-    ["ipinfo", "OSINT"],
-    ["phoneinfo", "OSINT"],
-    ["urlinfo", "OSINT"],
-    ["ia", "IA"],
+principales_data = [
+    ["reload", "Reinicia su sesión de termux."],
 ]
 
 headers = ["Comando", "Descripción"]
 
-print(Style.BRIGHT + Fore.RED + tabulate(menu_data, headers, tablefmt="fancy_grid"), Style.RESET_ALL)
+# Tabla de comandos principales
+print(Style.BRIGHT + Fore.RED + tabulate(principales_data, headers=("Comando", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
+
+utilidades_data = [
+    ["ia", "Chatbot de inteligencia artificial."],
+]
+
+Tabla de comandos de utilidad
+print(Style.BRIGHT + Fore.RED + tabulate(utiliades_data, headers=("Comando", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
+
+osint_data = [
+    ["ipinfo", "Obtiene la información de una ip."],
+    ["phoneinfo", "Obtiene la información de un numero de teléfono."],
+    ["urlinfo", "Obtiene información sobre una url."],
+    ["metadatainfo", "Recupera los metadatos de una imagen, audio o video."],
+]
+
+Tabla de comandos osint
+print(Style.BRIGHT + Fore.RED + tabulate(osint_data, headers=("Comando", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
