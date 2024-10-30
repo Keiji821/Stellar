dc756dc79c6213653f2bfe6888e08143dcfc84bb
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-29 20:11:48 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -5,25 +5,16 @@ from tabulate import tabulate
 init()
 
 # Tabla de comandos principales
-principales_data = [
+comandos_data = [
+    ["Comandos principales", "Información"],
     ["reload", "Reinicia su sesión de termux."],
-]
-
-print(Style.BRIGHT + Fore.CYAN + tabulate(principales_data, headers=("Comando principales", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
-
-# Tabla de comandos de utilidad
-utilidades_data = [
+    ["Comandos de utilidades", "Información"],
     ["ia", "Chatbot de inteligencia artificial."],
-]
-
-print(Style.BRIGHT + Fore.CYAN + tabulate(utilidades_data, headers=("Comando de utilidades", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
-
-# Tabla de comandos osint
-osint_data = [
+    ["Comandos de osint", "Información"],
     ["ipinfo", "Obtiene la información de una ip."],
     ["phoneinfo", "Obtiene la información de un numero de teléfono."],
     ["urlinfo", "Obtiene información sobre una url."],
     ["metadatainfo", "Recupera los metadatos de una imagen, audio o video."],
 ]
 
-print(Style.BRIGHT + Fore.CYAN + tabulate(osint_data, headers=("Comandos osint", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
+print(Style.BRIGHT + Fore.CYAN + tabulate(comandos_data, headers=("Comandos", "Información"), tablefmt="fancy_grid"), Style.RESET_ALL)
