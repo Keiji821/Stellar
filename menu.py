8ca45589e34e8face6822008d7b5c0affd77a183
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-30 21:44:25 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -5,19 +5,19 @@ init()
 print("")
 print(Fore.WHITE + Back.GREEN + "Comandos de Stellar", Style.RESET_ALL)
 print("")
-print(Fore.WHITE + Back.GREEN + "PRINCIPALES", Style.RESET_ALL)
+print(Fore.WHITE + Back.GREEN + "  PRINCIPALES", Style.RESET_ALL)
 print("""
 
 """)
 
 print("")
-print(Fore.WHITE + Back.GREEN + "UTILIDADES", Style.RESET_ALL)
+print(Fore.WHITE + Back.GREEN + "  UTILIDADES", Style.RESET_ALL)
 print("""
 
 
 """)
 
-print(Fore.WHITE + Back.GREEN + "OSINT", Style.RESET_ALL)
+print(Fore.WHITE + Back.GREEN + "  OSINT", Style.RESET_ALL)
 print("""
 
 """)
