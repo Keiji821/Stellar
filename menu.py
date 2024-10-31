eca0cf864d0eba7cb323589a3e5c27e3d11828c2
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-30 21:36:39 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -3,3 +3,20 @@ from colorama import init, Fore, Back, Style
 init()
 
 print(Fore.WHITE + Back.GREEN + "Comandos de Stellar")
+print("")
+print("PRINCIPALES")
+print("""
+
+""")
+
+print("")
+print("UTILIDADES")
+print("""
+
+
+""")
+
+print("OSINT")
+print("""
+
+""")
