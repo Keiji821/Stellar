8ff4ae7c40524cb038b17a80f4d576bc5bc99d07
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-30 21:39:54 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -2,21 +2,22 @@ from colorama import init, Fore, Back, Style
 
 init()
 
-print(Fore.WHITE + Back.GREEN + "Comandos de Stellar")
 print("")
-print("PRINCIPALES")
+print(Fore.WHITE + Back.GREEN + "Comandos de Stellar", Style.RESET_ALL)
+print("")
+print(Fore.WHITE + Back.GREEN + "PRINCIPALES", Style.RESET_ALL)
 print("""
 
 """)
 
 print("")
-print("UTILIDADES")
+print(Fore.WHITE + Back.GREEN + "UTILIDADES", Style.RESET_ALL)
 print("""
 
 
 """)
 
-print("OSINT")
+print(Fore.WHITE + Back.GREEN + "OSINT", Style.RESET_ALL)
 print("""
 
 """)
