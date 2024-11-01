7c44747986a633c9b8063bee28035b306b29c7a7
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-10-31 23:47:22 -0400

    Actualizar urlinfo.py

--- osint/urlinfo.py
+++ osint/urlinfo.py
@@ -17,7 +17,7 @@ def analyze_url(url):
         meta_description = soup.find('meta', attrs={'name': 'description'})
         meta_keywords = soup.find('meta', attrs={'name': 'keywords'})
 
-        nmap_output = subprocess.check_output(['nmap', '-v', '-A', url])
+        nmap_output = subprocess.check_output(['nmap', '-v', '-A', ip_address])
 
         open_ports = []
         for line in nmap_output.decode('utf-8').splitlines():
