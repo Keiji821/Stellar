8a79f0779d0e5769efe6b72c1c4401b8065f1a0d
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-01 11:09:49 -0400

    Actualizar urlinfo.py

--- osint/urlinfo.py
+++ osint/urlinfo.py
@@ -29,18 +29,13 @@ def analyze_url(url):
             if 'open' in line:
                 open_ports.append(line.split()[0])
 
-        print("URL", url)
-        print("Título", title)
-        print("Meta Description", meta_description['content'] if meta_description else '')
-        print("Meta Keywords", meta_keywords['content'] if meta_keywords else '')
-        print("Puertos abiertos", ', '.join(open_ports))
-        print("Dirección IP", ip_address)
-        print("Servidor", server_info)
-        grid = Table.grid(expand=True)
-        grid.add_column()
-        grid.add_column(justify="right")
-        grid.add_row("Raising shields", "[bold magenta]COMPLETED [green]:heavy_check_mark:")
-        print(grid)
+        print("URL: ", url)
+        print("Título: ", title)
+        print("Descripción: ", meta_description['content'] if meta_description else '')
+        print("Palabras clave: ", meta_keywords['content'] if meta_keywords else '')
+        print("Puertos abiertos: ", ', '.join(open_ports))
+        print("Dirección IP: ", ip_address)
+        print("Servidor: ", server_info)
 
     except requests.exceptions.RequestException as e:
         print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
