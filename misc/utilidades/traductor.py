329d5d5e54d9cd0abb6676924784b9b08e4c4c0e
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-01 14:46:47 -0400

    Actualizar traductor.py

--- misc/utilidades/traductor.py
+++ misc/utilidades/traductor.py
@@ -1,17 +1,22 @@
 from googletrans import Translator
+from rich import print
+from rich.console import Console
+from rich.markdown import Markdown
+
+console = Console()
 
 def traducir(texto, idioma_destino):
-    traductor = Translator()
     try:
+        traductor = Translator()
         resultado = traductor.translate(texto, dest=idioma_destino)
         return resultado.text
     except Exception as e:
         return f"Error al traducir: {e}"
 
 if __name__ == "__main__":
-    print("Bienvenido al Traductor en Termux")
+    console.print("Bienvenido al Traductor en Termix", style="bold green")
     texto = input("Introduce el texto que deseas traducir: ")
     idioma_destino = input("Introduce el código del idioma de destino (ej. 'en' para inglés, 'es' para español): ")
 
     traduccion = traducir(texto, idioma_destino)
-    print(f"Traducción: {traduccion}") 
+    console.print(f"Traducción: {traduccion}", style="bold blue")
