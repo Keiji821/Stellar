461e97c02ae16ffe279a0cdb4318ee236240ce03
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-02 12:18:38 -0400

    Actualizar ipinfo.py

--- osint/ipinfo.py
+++ osint/ipinfo.py
@@ -24,50 +24,76 @@ except Exception as e:
     console.print(f"[bold red]Error inesperado: {e}[/bold red]")
     exit()
 
-table_data1 = [
-    ("Red", data1.get("network", "No disponible")),
-    ("Tipo de IP", data1.get("version", "No disponible")),
-    ("TLD", data1.get("country_tld", "No disponible")),
-    ("ASN", data1.get("asn", "No disponible")),
-    ("Empresa", data1.get("org", "No disponible")),
-    ("RIR", data2.get("rir", "No disponible")),
-    ("Es una IP no autorizada", data2.get("is_bogon", "No disponible")),
-    ("Es un móvil", data2.get("is_mobile", "No disponible")),
-    ("Es un rastreador", data2.get("is_crawler", "No disponible")),
-    ("Es un centro de datos", data2.get("is_datacenter", "No disponible")),
-    ("Es una red Tor", data2.get("is_tor", "No disponible")),
-    ("Es un proxy", data2.get("is_proxy", "No disponible")),
-    ("Es una VPN", data2.get("is_vpn", "No disponible")),
-    ("Es una IP sospechosa", data2.get("is_abuser", "No disponible")),
- 
-    ("País", data1.get("country", "No disponible")),
-    ("Capital", data1.get("country_capital", "No disponible")),
-    ("Ciudad", data1.get("city", "No disponible")),
-    ("Región", data1.get("region", "No disponible")),
-    ("Código de región", data1.get("region_code", "No disponible")),
-    ("Nombre de país", data1.get("country_name", "No disponible")),
-    ("Código de país", data1.get("country_code", "No disponible")),
-    ("ISO3", data1.get("country_code_iso3", "No disponible")),
-    ("Código de continente", data1.get("continent_code", "No disponible")),
-    ("En Europa", data1.get("in_eu", "No disponible")),
-    ("Código postal", data1.get("postal", "No disponible")),
-    ("Latitud", data1.get("latitude", "No disponible")),
-    ("Longitud", data1.get("longitude", "No disponible")),
-    ("Zona horaria", data1.get("timezone", "No disponible")),
-    ("UTC Offset", data1.get("utc_offset", "No disponible")),
-    ("Código de llamada", data1.get("country_calling_code", "No disponible")),
-    ("Moneda", data1.get("currency", "No disponible")),
-    ("Nombre de moneda", data1.get("currency_name", "No disponible")),
-    ("Idioma", data1.get("languages", "No disponible")),
-    ("Área del país", data1.get("country_area", "No disponible")),
-    ("Población del país", data1.get("country_population", "No disponible")),
-]
-
 table1 = Table(title="Información de la IP", title_justify="center", title_style="bold magenta")
 table1.add_column("Información", style="cyan", no_wrap=True)
 table1.add_column("Valor", style="magenta")
 
-for info, valor in table_data1:
-    table1.add_row(info, str(valor))
+table.add_row("Red", data1.get("network", "No disponible"))
 
-console.print(table1)
+table.add_row("Tipo de IP", data1.get("version", "No disponible"))
+
+table.add_row("TLD", data1.get("country_tld", "No disponible"))
+
+table.add_row("ASN", data1.get("asn", "No disponible"))
+
+table.add_row("Empresa", data1.get("org", "No disponible"))
+
+table.add_row("RIR", data2.get("rir", "No disponible"))
+
+table.add_row("Es una IP no autorizada", data2.get("is_bogon", "No disponible"))
+
+table.add_row("Es un móvil", data2.get("is_mobile", "No disponible")),
+
+table.add_row("Es un rastreador", data2.get("is_crawler", "No disponible"))
+    
+table.add_row("Es un centro de datos", data2.get("is_datacenter", "No disponible"))
+
+table.add_row("Es una red Tor", data2.get("is_tor", "No disponible"))
+
+table.add_row("Es un proxy", data2.get("is_proxy", "No disponible"))
+
+table.add_row("Es una VPN", data2.get("is_vpn", "No disponible"))
+
+table.add_row("Es una IP sospechosa", data2.get("is_abuser", "No disponible"))
+ 
+table.add_row("País", data1.get("country", "No disponible"))
+
+table.add_row("Capital", data1.get("country_capital", "No disponible"))
+
+table.add_row("Ciudad", data1.get("city", "No disponible"))
+
+table.add_row("Región", data1.get("region", "No disponible"))
+
+table.add_row("Código de región", data1.get("region_code", "No disponible"))
+
+table.add_row("Nombre de país", data1.get("country_name", "No disponible"))
+
+table.add_row("Código de país", data1.get("country_code", "No disponible"))
+    
+table.add_row("ISO3", data1.get("country_code_iso3", "No disponible"))
+
+table.add_row("Código de continente", data1.get("continent_code", "No disponible"))
+
+table.add_row("En Europa", data1.get("in_eu", "No disponible"))
+
+table.add_row("Código postal", data1.get("postal", "No disponible"))
+
+table.add_row("Latitud", data1.get("latitude", "No disponible"))
+
+table.add_row("Longitud", data1.get("longitude", "No disponible"))
+    
+table.add_row("Zona horaria", data1.get("timezone", "No disponible"))
+
+table.add_row("UTC Offset", data1.get("utc_offset", "No disponible"))
+
+table.add_row("Código de llamada", data1.get("country_calling_code", "No disponible"))
+
+table.add_row("Moneda", data1.get("currency", "No disponible"))
+
+table.add_row("Nombre de moneda", data1.get("currency_name", "No disponible"))
+
+table.add_row("Idioma", data1.get("languages", "No disponible"))
+
+table.add_row("Área del país", data1.get("country_area", "No disponible"))
+
+table.add_row("Población del país", data1.get("country_population", "No disponible"))
