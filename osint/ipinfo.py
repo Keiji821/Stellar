d20074c2f392ece3fe37d64eaa004ff4727e5a41
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-02 12:48:16 -0400

    Actualizar ipinfo.py

--- osint/ipinfo.py
+++ osint/ipinfo.py
@@ -26,7 +26,7 @@ except Exception as e:
 
 table = Table(title="Información de la IP", title_justify="center", title_style="bold magenta")
 
-table.add_column("Información", style="cyan", no_wrap=True)
+table.add_column("Información de red", style="cyan", no_wrap=True)
 table.add_column("Valor", style="magenta")
 
 table.add_row("Red", str(data1.get("network", "No disponible")),)
@@ -43,6 +43,10 @@ table.add_row("Es una red Tor", str(data2.get("is_tor", "No disponible")),)
 table.add_row("Es un proxy", str(data2.get("is_proxy", "No disponible")),)
 table.add_row("Es una VPN", str(data2.get("is_vpn", "No disponible")),)
 table.add_row("Es una IP sospechosa", str(data2.get("is_abuser", "No disponible")),)
+
+table.add_column("Información de red", style="cyan", no_wrap=True)
+table.add_column("Valor", style="magenta")
+
 table.add_row("País", str(data1.get("country", "No disponible")),)
 table.add_row("Capital", str(data1.get("country_capital", "No disponible")),)
 table.add_row("Ciudad", str(data1.get("city", "No disponible")),)
