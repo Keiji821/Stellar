822452a42b7e62d096beeb6c0b7c3e566df50634
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-04 21:43:49 -0400

    Actualizar menu.py

--- menu.py
+++ menu.py
@@ -1,31 +1,31 @@
 from rich.console import Console
 from rich.markdown import Markdown
-from colorama import init, Fore, Back, Style
 
-init()
+console = Console()
 
-print("")
+console.print(" ")
 MARKDOWN = """
 # Comandos de Stellar
 """
 console = Console()
 md = Markdown(MARKDOWN)
 console.print(md)
-print("")
-print(Style.BRIGHT + Fore.GREEN + "  PRINCIPALES", Style.RESET_ALL)
-print("   reload", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Reinicia su sesion de termux/bash.")
 
-print("")
-print(Style.BRIGHT + Fore.GREEN + "  UTILIDADES", Style.RESET_ALL)
-print("   ia", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Un pequeño servicio de inteligencia artificial.")
+console.print(" ")
+console.print("  PRINCIPALES")
+console.print("   reload", ">", "Reinicia su sesion de termux/bash.")
 
-print("")
-print(Style.BRIGHT + Fore.GREEN + "  OSINT", Style.RESET_ALL)
-print("   ipinfo", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Obtiene la información de una ip, ya sea IPV4 o IPV6.")
-print("   phoneinfo", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Obtiene la información de un numero de teléfono.")
-print("   urlinfo", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Obtiene información de una url o enlace sin necesidad de entrar.")
-print("   metadatainfo", Style.BRIGHT + Fore.YELLOW + ">", Style.RESET_ALL + "Recupera los metadatos de una imagen, archivo o video.")
+console.print(" ")
+console.print("  UTILIDADES")
+console.print("   ia", ">", "Un pequeño servicio de inteligencia artificial.")
 
-print("")
-print(Style.BRIGHT + Fore.WHITE + Back.GREEN + "CTRL + Z", Style.RESET_ALL + Fore.GREEN + " Esto detendrá cualquier comando o preceso existente.", Style.RESET_ALL)
-print("")
+console.print(" ")
+console.print("  OSINT")
+console.print("   ipinfo", ">", "Obtiene la información de una ip, ya sea IPV4 o IPV6.")
+console.print("   phoneinfo", ">", "Obtiene la información de un numero de teléfono.")
+console.print("   urlinfo", ">", "Obtiene información relevante de una url o enlace.")
+console.print("   metadatainfo", ">", "Recupera los metadatos de una imagen, archivo o video.")
+
+console.print(" ")
+console.print("CTRL + Z", " Esto detendrá cualquier comando o preceso existente.")
+console.print(" ")
