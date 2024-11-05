e95d5b7198691b761bd4817495843c90560815f5
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-04 22:06:59 -0400

    Actualizar iahttp.py

--- misc/utilidades/iahttp.py
+++ misc/utilidades/iahttp.py
@@ -1,7 +1,7 @@
 import requests
-import textwrap
-import os
+from rich.progress import Progress
 from rich.console import Console
+import textwrap
 from rich.table import Table
 from rich.markdown import Markdown
 
@@ -10,38 +10,36 @@ console = Console()
 API_KEY = "Kastg_fKlIk2c1LRc8969in2g9_free"
 
 def get_ai_response(user_input):
-    try:
-        url = f"https://api.kastg.xyz/api/ai/fast-llamaV3-large?key={API_KEY}&prompt={user_input}"
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()["result"][0]["response"]
-    except requests.exceptions.RequestException as e:
-        console.print("[bold red] Error: " + str(e) + "[/bold red]")
-        return None
+    with Progress() as progress:
+        task = progress.add_task("[red]Making request...", total=100)
+        try:
+            url = f"https://api.kastg.xyz/api/ai/fast-llamaV3-large?key={API_KEY}&prompt={user_input}"
+            response = requests.get(url)
+            progress.update(task, advance=50)
+            response.raise_for_status()
+            progress.update(task, advance=50)
+            return response.json()["result"][0]["response"]
+        except requests.exceptions.RequestException as e:
+            console.print("[bold red] Error: " + str(e) + "[/bold red]")
+            return None
 
 def print_ai_response(response):
     wrapped_response = textwrap.fill(response, width=50)
     print(" ")
     table = Table(title="LlaMa IA", title_justify="center", title_style="bold magenta")
     table.add_column("Respuesta", style="white", no_wrap=False)
-    
+
     MARKDOWN = wrapped_response
     md = Markdown(MARKDOWN)
     table.add_row(md)
     console.print(table)
     console.print(" ")
 
-def execute_command(command):
-    print(f"Ejecutando comando: {command}")
-    os.system(command)
-
 while True:
     user_input = console.input("[bold green]> [/bold green]")
     response = get_ai_response(user_input)
     if response:
         print_ai_response(response)
-        if "ejecutar " in response:
-            command = response.split("ejecutar ")[1]
-            execute_command(command)
     else:
         console.print("[bold red]Error[/bold red]")
+
