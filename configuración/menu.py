e47ca21e95956f09aa81410eb25e4ff56ee05177
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-09 03:42:43 -0400

    Actualizar menu.py

--- configuración/menu.py
+++ configuración/menu.py
@@ -7,25 +7,25 @@ from rich import print
 console = Console()
 
 console.print(" ")
-console.print(Panel(":gear: Versión V1.0.", title="[bold red]Comandos de Stellar[/bold red]"), style="bold yellow")
+console.print(Panel(":dvd: Versión V1.0.", title="[bold red]Comandos de Stellar[/bold red]"), style="bold yellow")
 
 console.print(" ")
 
 categories = [
     {"title": "SISTEMA", "commands": [
-        {"name": ":dvd: reload", "description": "Esto recargara el banner y actualizará stellar."},
-        {"name": "clear", "description": "Esto limpiara la pantalla de su terminal."},
-        {"name": "bash", "description": "Esto reiniciara su terminal."}
+        {"name": ":gear: reload", "description": "Esto recargara el banner y actualizará stellar."},
+        {"name": ":gear: clear", "description": "Esto limpiara la pantalla de su terminal."},
+        {"name": ":gear: bash", "description": "Esto reiniciara su terminal."}
     ]},
     {"title": "UTILIDADES", "commands": [
-        {"name": "ia", "description": "Un pequeño servicio de inteligencia artificial mediante un api gratis."}
+        {"name": ":gear: ia", "description": "Un pequeño servicio de inteligencia artificial mediante un api gratis."}
     ]},
     {"title": "OSINT", "commands": [
-        {"name": "ipinfo", "description": "Obtiene la información de una ip, ya sea IPV4 o IPV6."},
-        {"name": "phoneinfo", "description": "Obtiene la información de un número de teléfono."},
-        {"name": "urlinfo", "description": "Obtiene información relevante de una url o enlace."},
-        {"name": "metadatainfo", "description": "Recupera los metadatos de una imagen, archivo o video."},
-        {"name": "emailsearch", "description": "Busca correos electrónicos con el nombre y apellido proporcionados."}
+        {"name": ":gear: ipinfo", "description": "Obtiene la información de una ip, ya sea IPV4 o IPV6."},
+        {"name": ":gear: phoneinfo", "description": "Obtiene la información de un número de teléfono."},
+        {"name": ":gear: urlinfo", "description": "Obtiene información relevante de una url o enlace."},
+        {"name": ":gear: metadatainfo", "description": "Recupera los metadatos de una imagen, archivo o video."},
+        {"name": ":gear: emailsearch", "description": "Busca correos electrónicos con el nombre y apellido proporcionados."}
     ]}
 ]
 
