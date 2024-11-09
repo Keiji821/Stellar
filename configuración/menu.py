a250bb18efee6fd28615daaceac67029e98ff3de
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-09 16:35:07 -0400

    Actualizar menu.py

--- configuración/menu.py
+++ configuración/menu.py
@@ -7,8 +7,8 @@ from rich import print
 console = Console()
 
 console.print(" ")
-markdown = Markdown("/n Hola")
-console.print(f"[code]{markdown}[/code]")
+md = Markdown("[code]Hola[/code]")
+console.print(md)
 console.print(Panel(":dvd: Versión V1.0.", title="[bold red]Comandos de Stellar[/bold red]"), style="bold yellow")
 
 console.print(" ")
