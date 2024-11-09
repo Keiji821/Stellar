235ce624377f448099f25703c90a20017c0e07ea
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-09 17:41:50 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -258,7 +258,7 @@ craneo2 = """
 
 banners = random.choice([f"[code]{craneo1}[/code]", blackhole1, craneo2, planet1, chica1, chica2, chica3, chica4, chica5])
 
-stellar = """[bold red]
+console.print("""[bold red]
  ________  _________  _______   ___       ___       ________  ________     
 |\   ____\|\___   ___\\  ___ \ |\  \     |\  \     |\   __  \|\   __  \    
 \ \  \___|\|___ \  \_\ \   __/|\ \  \    \ \  \    \ \  \|\  \ \  \|\  \   
@@ -267,8 +267,7 @@ stellar = """[bold red]
     ____\_\  \   \ \__\ \ \_______\ \_______\ \_______\ \__\ \__\ \__\\ _\ 
    |\_________\   \|__|  \|_______|\|_______|\|_______|\|__|\|__|\|__|\|__|
    \|_________|                                                            
-[/bold green]"""
-console.print(stellar)
+[/bold green]""")
 spinner = Spinner("dots", text="Presiona [Enter] para continuar", style="yellow")
 with console.status(spinner):
     input("")
