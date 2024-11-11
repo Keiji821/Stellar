783f42c80ef7bacc58a0f25c4537422772c2794c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-11 14:01:41 -0400

    Actualizar myip.py

--- misc/utilidades/myip.py
+++ misc/utilidades/myip.py
@@ -7,16 +7,17 @@ import socket
 
 console = Console()
 
+ip = socket.gethostbyname(socket.gethostname())
+
 with Progress(SpinnerColumn("dots")) as progress:
     task = progress.add_task("[red]Cargando...")
     try:
-        response1 = requests.get(f'https://ipapi.co/', socket.gethostbyname(socket.gethostname())
-, '/json/')
+        response1 = requests.get(f'https://ipapi.co/{ip}/json/')
         progress.update(task, advance=20)
         response1.raise_for_status()
         data1 = response1.json()
 
-        response2 = requests.get(f'https://api.ipapi.is/?ip=', socket.gethostbyname(socket.gethostname())
+        response2 = requests.get(f'https://api.ipapi.is/?ip={ip}')
 )
         progress.update(task, advance=30)
         response2.raise_for_status()
