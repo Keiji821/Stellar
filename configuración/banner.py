703442f9a5f4bf6d5209a3aea6408e7ffa5ef85c
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-11 19:04:56 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -306,7 +306,10 @@ console.print(
 f"""[bold green]OS: [/bold green][bold white]{os_version}[/bold white]
 [bold green]Sistema: [/bold green][bold white]{system_info}[/bold white]""", justify="left")
 
-console.print(f"[bold green]Fecha: [/bold green][bold white]{date_string}[/bold white]", "[bold green]Hora: [/bold green][bold white]{hour_string}[/bold white]", justify="right")
+console.print(
+
+f"""[bold green]Fecha: [/bold green][bold white]{date_string}[/bold white]
+[bold green]Hora: [/bold green][bold white]{hour_string}[/bold white]""", justify="right")
 
 console.print(f"[bold {colores}]{banners}[/bold {colores}]", justify="center")
 
