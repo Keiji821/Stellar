a76d0fe9a1255184d104c3e905b156caa8f020a0
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-11 18:39:08 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -305,11 +305,7 @@ os.system("clear")
 
 colores = random.choice(["red", "magenta", "yellow", "blue", "cyan"])
 
-table = Table(title=" ", title_justify="center", title_style="bold red")
-
-table.add_column("I", style="bold" + colores, no_wrap=False)
-
-table.add_row(banners)
+console.print(f"[bold {colores}]{banners}[/bold {colores}]")
 
 console.print("")
 MARKDOWN = "> **Stellar V1.0**"
