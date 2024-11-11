6b0404f930b71a213242d68223e1c476b716214b
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-11 14:24:57 -0400

    Actualizar myip.py

--- misc/utilidades/myip.py
+++ misc/utilidades/myip.py
@@ -14,15 +14,17 @@ def get_real_ip():
     s.close()
     return ip
 
+ip = get_real_ip()
+
 with Progress(SpinnerColumn("dots")) as progress:
     task = progress.add_task("[red]Cargando...")
     try:
-        response1 = requests.get(f'https://ipapi.co/', get_real_ip(),'/json/')
+        response1 = requests.get(f'https://ipapi.co/{ip}/json/')
         progress.update(task, advance=20)
         response1.raise_for_status()
         data1 = response1.json()
 
-        response2 = requests.get(f'https://api.ipapi.is/?ip=', get_real_ip())
+        response2 = requests.get(f'https://api.ipapi.is/?ip={ip}')
         progress.update(task, advance=30)
         response2.raise_for_status()
         data2 = response2.json()
