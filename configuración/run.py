7c64bdb86bd68366c1ef718bc09a4fe7f11a2bf2
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-12 10:44:14 -0400

    Actualizar run.py

--- configuración/run.py
+++ configuración/run.py
@@ -1,45 +1,36 @@
 from rich.console import Console
-from rich.progress import Progress, BarColumn, TextColumn
+from rich.progress import Progress, BarColumn, TextColumn, SpinnerColumn
 import os
 import time
-import itertools
 
 console = Console()
 
-def red_green_cycle():
-    colors = ["red", "green"]
-    while True:
-        for color in colors:
-            yield color
-
-color_cycle = red_green_cycle()
+commands = [
+    "clear",
+    "pkill tor &>/dev/null &",
+    "pkill cloudflared &>/dev/null &",
+    "sleep 1",
+    "export ALL_PROXY=socks5h://localhost:9050",
+    "tor &>/dev/null &",
+    "sleep 1",
+    "cloudflared --url Stellar &>/dev/null &",
+    "cd && cd Stellar && bash update.sh &>/dev/null &",
+    "git pull --force",
+    "cp ~/Stellar/configuración/.bash_profile ~/."
+]
 
 with Progress(
+    SpinnerColumn(),
     TextColumn("[progress.description]{task.description}"),
-    BarColumn(bar_width=None, style="bold"),
+    BarColumn(bar_width=None, style="green"),
     TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
     console=console,
-    refresh_per_second=30,
+    refresh_per_second=10,
 ) as progress:
     
-    tasks = []
-    for i in range(14):
-        color = next(color_cycle)
-        tasks.append(progress.add_task(f"[{color}]Tarea {i+1}", total=1000))
+    task = progress.add_task("[cyan]Ejecución de comandos...", total=len(commands))
 
-    while not progress.finished:
-        for i, task in enumerate(tasks):
-            color = next(color_cycle)
-            progress.update(task, description=f"[{color}]Tarea {i+1}", advance=1.5)
-            
-        os.system("clear")
-        os.system("pkill tor &>/dev/null &")
-        os.system("pkill cloudflared &>/dev/null &")
-        os.system("sleep 1")
-        os.system("export ALL_PROXY=socks5h://localhost:9050")
-        os.system("tor &>/dev/null &")
-        os.system("sleep 1")
-        os.system("cloudflared --url Stellar &>/dev/null &")
-        os.system("cd && cd Stellar && bash update.sh &>/dev/null &")
-        os.system("git pull --force")
-        os.system("cp ~/Stellar/configuración/.bash_profile ~/.")
+    for command in commands:
+        progress.update(task, description=f"[cyan]Ejecutando: {command}", advance=1)
+        os.system(command)
+        time.sleep(0.5)
