53e4119614d607a2a5588f751124378a0734e89d
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-12 22:48:21 -0400

    Actualizar menu.py

--- configuración/menu.py
+++ configuración/menu.py
@@ -35,8 +35,8 @@ for i, category in enumerate(categories):
         console.print(f"   [bold yellow]{command['name']}[/bold yellow]: {command['description']}")
     console.print(" ")
 
-console.print("[code][bold yellow]CTRL + Z │[/bold yellow][/code] Esto detendrá cualquier comando o proceso existente.")
+console.print("[code][bold yellow]CTRL + Z[/bold yellow][/code] Esto detendrá cualquier comando o proceso existente.")
 console.print(" ")
-console.print("[code][bold yellow]CTRL + C │[/bold yellow][/code] Esto forzará a cualquier proceso activo a que se detenga, debes pulsar varias veces las dos combinaciones las veces que haga falta hasta que se detenga el proceso.")
+console.print("[code][bold yellow]CTRL + C[/bold yellow][/code] Esto forzará a cualquier proceso activo a que se detenga, debes pulsar varias veces las dos combinaciones las veces que haga falta hasta que se detenga el proceso.")
 console.print(" ")
 console.print(" ")
