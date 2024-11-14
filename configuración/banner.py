eaf1d397f932d65cf82798316a807f024bde76ad
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-14 15:04:15 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -270,7 +270,7 @@ f"""[bold green]OS: [/bold green][bold white]{os_version}[/bold white]
 [bold green]Hora: [/bold green][bold white]{hour_string}[/bold white]
 [bold green]Tu ip anónima[bold red]/[/bold red]tu ip visible en el internet: [/bold green][bold white]{ip}[/bold white]""", justify="center")
 
-console.print(f"[code]{banners}[/code", justify="center", style=f"bold {colores}")
+console.print(f"[code]{banners}[/code]", justify="center", style=f"bold {colores}")
 
 
 console.print("[bold red]Stellar V1.0.0[/bold red]", justify="center")
