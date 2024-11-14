f633f7a9e291fab9cc213d997d45a221fec1a5fd
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-14 16:01:24 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -5,7 +5,7 @@ import platform
 import random
 import time
 import requests
-from pylolcat import lolcatify
+from lolcat import lolcatify
 from pyfiglet import Figlet
 from rich.console import Console
 from rich.markdown import Markdown
