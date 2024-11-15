f24e79458aca1571a1454789f09a7333623105d6
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-15 15:55:40 -0400

    Actualizar banner.py

--- configuración/banner.py
+++ configuración/banner.py
@@ -260,9 +260,9 @@ data = response.json()
 if data is not None:
     ip = data.get("network")
     if ip is None:
-        ip = "[bold red]La conexión de red es inestable.[/bold red]"
+        ip = "[bold red]El anonimizador no se ha iniciado[/bold red]"
 else:
-    ip = "Ha ocurrido un error de red, posible fallo al iniciar Stellar."
+    ip = "[bold red]Error de red[/bold red]"
 
 console.print(
 f"""[bold green]OS: [/bold green][bold white]{os_version}[/bold white]
