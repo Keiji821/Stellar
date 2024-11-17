6edb51cce7b6ee68ec0f3e4a2ac58888c59030cf
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-17 15:10:08 -0400

    Actualizar themes

---
+++ config/themes/ui_config.sh
@@ -0,0 +1,10 @@
+printf "${verde}"
+ read -p 'Ingrese el contenido: ' banner
+ echo "${banner}" > banner.txt
+ echo
+ printf "${gris}[$verde2✔$gris]${blanco2} Su banner personalizado se ha configurado correctamente!"
+ echo " "
+ echo " "
+ printf "${gris}[${verde}+${gris}] ${blanco2}Escriba ${verde}reload ${blanco2}para aplicar los cambios."
+ echo " " 
+ echo " "
