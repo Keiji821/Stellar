2d1ddef8ea89f1b329b71aa698d9c97b5535879a
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-17 15:11:20 -0400

    Actualizar ui_config.sh

--- config/themes/ui_config.sh
+++ config/themes/ui_config.sh
@@ -1,3 +1,24 @@
+# Definir colores
+
+gris="\033[1;30m"
+blanco="\033[0m"
+blanco2="\033[1;37m"
+rojo="\033[1;31m"
+rojo2="\033[31m"
+azul="\033[1;34m"
+azul2="\033[34m"
+azul_agua="\e[1;36m"
+azul_agua2="\e[36m"
+verde="\033[1;32m"
+verde2="\033[32m"
+morado="\033[1;35m"
+morado2="\033[35m"
+amarillo="\033[1;33m"
+amarillo2="\033[33m"
+cyan="\033[38;2;23;147;209m"
+
+# ui_config.sh
+
 printf "${verde}"
  read -p 'Ingrese el contenido: ' banner
  echo "${banner}" > banner.txt
