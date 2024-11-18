da3b914c6400b74cc4ae521d63370575948c5cbc
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-11-18 16:14:44 -0400

    Actualizar myip.py

--- misc/utilidades/myip.py
+++ misc/utilidades/myip.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from os import system
 from rich.progress import Progress, SpinnerColumn
 from rich.console import Console
 from rich.table import Table
@@ -10,11 +11,15 @@ console = Console()
 def obtener_info_dispositivo(ip):
     try:
         response = requests.get(f'https://ipapi.co/{ip}/json/')
-        response.raise_for_status()
-        data = response.json()
-        return data.get('is_mobile', 'Desconocido'), data.get('os', 'Desconocido')
-    except requests.RequestException:
-        return 'Error al obtener', 'Desconocido'
+        if response.status_code == 200:
+            data = response.json()
+            user_agent = data.get('user_agent', 'Desconocido')
+            device = data.get('device', 'Desconocido')
+            operating_system = data.get('os', 'Desconocido')
+            return user_agent, device, operating_system
+        return 'Desconocido', 'Desconocido', 'Desconocido'
+    except requests.exceptions.RequestException:
+        return 'Error al obtener', 'Desconocido', 'Desconocido'
 
 def obtener_puerto_remoto(ip):
     try:
@@ -24,41 +29,58 @@ def obtener_puerto_remoto(ip):
     except socket.error:
         return 'Desconocido'
 
-def obtener_ip_publica():
-    comandos = [
-        "(command -v dig &> /dev/null && (dig +short @ident.me || dig +short @tnedi.me))",
-        "(command -v nc &> /dev/null && (nc ident.me 23 < /dev/null || nc tnedi.me 23 < /dev/null))",
-        "(command -v curl &> /dev/null && (curl -sf ident.me || curl -sf tnedi.me))",
-        "(command -v wget &> /dev/null && (wget -qO- ident.me || wget -qO- tnedi.me))"
-    ]
-    
-    for comando in comandos:
-        ip = os.popen(comando).read().strip()
-        if ip:
-            return ip
-    console.print("[bold red]Error: No se pudo obtener la IP pública.[/bold red]")
-    return None
+while True:
+    os.system("""
+IP=$(
+  (command -v dig &> /dev/null &&
+    (dig +short @ident.me ||
+     dig +short @tnedi.me)) ||
+  (command -v nc &> /dev/null &&
+    (nc ident.me 23 < /dev/null ||
+     nc tnedi.me 23 < /dev/null)) ||
+  (command -v curl &> /dev/null &&
+    (curl -sf ident.me ||
+     curl -sf tnedi.me)) ||
+  (command -v wget &> /dev/null &&
+    (wget -qO- ident.me ||
+     wget -qO- tnedi.me)) ||
+  (command -v openssl &> /dev/null &&
+    (openssl s_client -quiet -connect ident.me:992 2> /dev/null ||
+     openssl s_client -quiet -connect tnedi.me:992 2> /dev/null)) ||
+  (command -v ssh &> /dev/null &&
+    (ssh -qo StrictHostKeyChecking=accept-new ident.me ||
+     ssh -qo StrictHostKeyChecking=accept-new tnedi.me)) ||
+  (echo "Could not find public IP through api.ident.me" >&2
+   exit 42)
+)
+echo $IP > ip_address.txt""")
 
-def mostrar_info_ip(ip):
+    with open("ip_address.txt", "r") as f:
+        IpQuery = f.read().strip()
+    if not IpQuery:
+        console.print("[bold red]Error[/bold red]")
+        continue
+
     with Progress(SpinnerColumn("dots")) as progress:
         task = progress.add_task("[red]Cargando...")
-
         try:
-            response1 = requests.get(f'https://ipapi.co/{ip}/json/')
-            progress.update(task, advance=30)
+            response1 = requests.get(f'https://ipapi.co/{IpQuery}/json/')
+            progress.update(task, advance=20)
             response1.raise_for_status()
             data1 = response1.json()
 
-            response2 = requests.get(f'https://api.ipapi.is/?ip={ip}')
-            progress.update(task, advance=70)
+            response2 = requests.get(f'https://api.ipapi.is/?ip={IpQuery}')
+            progress.update(task, advance=30)
             response2.raise_for_status()
             data2 = response2.json()
+            progress.update(task, advance=50)
 
-            es_movil, sistema_operativo = obtener_info_dispositivo(ip)
-            puerto_remoto = obtener_puerto_remoto(ip)
+            user_agent, device, operating_system = obtener_info_dispositivo(IpQuery)
+            puerto_remoto = obtener_puerto_remoto(IpQuery)
 
+            console.print(" ")
             table = Table(title="Datos de la IP", title_justify="center", title_style="bold red")
-            table.add_column("Información", style="green")
+            table.add_column("Información", style="green", no_wrap=False)
             table.add_column("Valor", style="white")
 
             table.add_row("[bold underline]Información de Red[/bold underline]", "")
@@ -69,11 +91,20 @@ def mostrar_info_ip(ip):
             table.add_row("Empresa", str(data1.get("org", "No disponible")))
             table.add_row("RIR", str(data2.get("rir", "No disponible")))
             table.add_row("Es una IP no autorizada", str(data2.get("is_bogon", "No disponible")))
-            table.add_row("Es un móvil", str(data1.get("is_mobile", "No disponible")))
+            table.add_row("Es un móvil", str(data2.get("is_mobile", "No disponible")))
             table.add_row("Es un rastreador", str(data2.get("is_crawler", "No disponible")))
             table.add_row("Es un centro de datos", str(data2.get("is_datacenter", "No disponible")))
             table.add_row("Es una red Tor", str(data2.get("is_tor", "No disponible")))
             table.add_row("Es un proxy", str(data2.get("is_proxy", "No disponible")))
+            table.add_row("Es una VPN", str(data2.get("is_vpn", "No disponible")))
+            table.add_row("Es una IP sospechosa", str(data2.get("is_abuser", "No disponible")))
+            table.add_row("Nivel de fraude", str(data2.get("asn", {}).get("abuser_score", "No disponible")))
+            table.add_row("Es una IP activa", str(data2.get("asn", {}).get("active", "No disponible")))
+            table.add_row("Dominio", str(data2.get("asn", {}).get("domain", "No disponible")))
+            table.add_row("Fecha de creación", str(data2.get("asn", {}).get("created", "No disponible")))
+            table.add_row("Correo de la empresa", str(data2.get("abuse", {}).get("email", "No disponible")))
+
+            table.add_row("", "")
 
             table.add_row("[bold underline]Información Geográfica[/bold underline]", "")
             table.add_row("País", str(data1.get("country", "No disponible")))
@@ -83,18 +114,33 @@ def mostrar_info_ip(ip):
             table.add_row("Código de región", str(data1.get("region_code", "No disponible")))
             table.add_row("Nombre de país", str(data1.get("country_name", "No disponible")))
             table.add_row("Código de país", str(data1.get("country_code", "No disponible")))
-
+            table.add_row("ISO3", str(data1.get("country_code_iso3", "No disponible")))
+            table.add_row("Código de continente", str(data1.get("continent_code", "No disponible")))
+            table.add_row("En Europa", str(data1.get("in_eu", "No disponible")))
+            table.add_row("Código postal", str(data1.get("postal", "No disponible")))
+            table.add_row("Latitud", str(data1.get("latitude", "No disponible")))
+            table.add_row("Longitud", str(data1.get("longitude", "No disponible")))
+            table.add_row("Zona horaria", str(data1.get("timezone", "No disponible")))
+            table.add_row("UTC Offset", str(data1.get("utc_offset", "No disponible")))
+            table.add_row("Código de llamada", str(data1.get("country_calling_code", "No disponible")))
+            table.add_row("Moneda", str(data1.get("currency", "No disponible")))
+            table.add_row("Nombre de moneda", str(data1.get("currency_name", "No disponible")))
+            table.add_row("Idioma", str(data1.get("languages", "No disponible")))
+            table.add_row("Área del país", str(data1.get("country_area", "No disponible")))
+            table.add_row("Población del país", str(data1.get("country_population", "No disponible")))
+            table.add_row(" ", " ")
             table.add_row("[bold underline]Información adicional[/bold underline]", "")
-            table.add_row("Es Móvil", es_movil)
-            table.add_row("Sistema Operativo", sistema_operativo)
+            table.add_row("User Agent", user_agent)
+            table.add_row("Dispositivo", device)
+            table.add_row("Sistema Operativo", operating_system)
             table.add_row("Puerto Remoto", str(puerto_remoto))
 
             console.print(table)
-        except requests.RequestException as e:
+            console.print(" ")
+            break
+        except requests.exceptions.RequestException as e:
             console.print(f"[bold red]Error de red: {e}[/bold red]")
+        except ValueError as e:
+            console.print(f"[bold red]Error de datos: {e}[/bold red]")
         except Exception as e:
             console.print(f"[bold red]Error inesperado: {e}[/bold red]")
-
-ip_publica = obtener_ip_publica()
-if ip_publica:
-    mostrar_info_ip(ip_publica)
