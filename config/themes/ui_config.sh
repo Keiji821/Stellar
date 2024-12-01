6410092fe277a6414b51751d7bf911dd886bc6c3
Keiji <127261198+Keiji821@users.noreply.github.com>
2024-12-01 00:54:23 -0400

    Actualizar ui_config.sh

--- config/themes/ui_config.sh
+++ config/themes/ui_config.sh
@@ -20,10 +20,7 @@ cyan="\033[38;2;23;147;209m"
 # ui_config.sh
 
 printf "${verde}"
-read -p "Ingrese el contenido:
-
-" banner
-sleep 10
+read -p "Ingrese el contenido: " banner
 echo "${banner}" > banner.txt
 printf "${verde}Elija la fuente para su banner"
 echo " "
