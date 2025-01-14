9d4828f15dd6feb2a4a7a836429d735d45301ea3
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-14 00:34:52 -0400

    Actualizar phoneinfo.py

--- osint/main/phoneinfo.py
+++ osint/main/phoneinfo.py
@@ -40,7 +40,7 @@ while True:
 
         table.add_row("País/dirección", country_name)
         table.add_row("Dirección 2:", str(data1.get("location")))
-        table.add_row("Empresa de teléfono", str(data1.get("carrier"), carrier_name))
+        table.add_row("Empresa de teléfono", str(data1.get("carrier"), "carrier_name"))
         table.add_row("Número de teléfono válido", is_valid)
         table.add_row("Código de país", str(country_code))
         table.add_row("Número nacional", str(national_number))
