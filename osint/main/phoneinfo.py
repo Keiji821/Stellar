3e98d8754c2c3acfc5345be6a5463c9c048e43f5
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-16 09:39:05 -0400

    Actualizar phoneinfo.py

--- osint/main/phoneinfo.py
+++ osint/main/phoneinfo.py
@@ -79,7 +79,7 @@ while True:
         
         print(" ")
         table = Table(title="Información del número de teléfono", title_justify="center", title_style="bold green")
-        table.add_column("[green]Información", style="bold green", no_wrap=False)
+        table.add_column("[green]Información", style="code", no_wrap=False)
         table.add_column("[green]Valor", style="code")
 
         table.add_row("[underline][bold green]Información geográfica[/bold green]")
