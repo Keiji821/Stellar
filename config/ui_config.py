ad7ad6aa98b831c0e85e3d16e938347210e0ca09
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-20 00:24:37 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -6,4 +6,18 @@ from os import system
 console = Console()
 
 
-console.print("")
+console.print("[code][bold green]Establecer banner", s)
+banner = input()
+console.print("[code][bold green]Establecer fuente para el banner de texto, deje en blanco si no coloco un banner de texto.")
+
+
+
+
+
+
+os.system(f"""
+cd
+cd .configs_stellar
+cd themes
+echo {banner} > banner.txt
+""")
