67f789297af454e40874b0066bed0a073157eebd
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-20 00:58:13 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -10,16 +10,19 @@ try:
     banner = input()
     if banner == "":
         banner = "Stellar"
+    time.sleep(5)
     
     console.print("[code][bold yellow]Establecer fuente para el banner de texto, deje en blanco si no coloco un banner de texto.", justify="center")
     banner_font = input()
     if banner_font == "":
         banner_font = "standard"
+    time.sleep(5)
         
     console.print("[code][bold yellow]Establezca el texto personalizado de la input", justify="center")
     input_text = input()
     if input_text == "":
         input_text = "Stellar"
+    time.sleep(5)
     
     os.system(f"""
 cd
@@ -30,6 +33,8 @@ echo {banner_font} > banner_font.txt
 echo {input_text} > input.txt
 cd
 """)
+
+    time.sleep(5)
 
     console.print("[code][bold green]Configuración realizada con éxito, escriba bash para que los cambios surtan efecto", justify="center")
 except Exception as e:
