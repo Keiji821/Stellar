470ba6972e8d68651b6a2bdc65f727565f1f09a6
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-20 00:56:26 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -5,22 +5,23 @@ from os import system
 
 console = Console()
 
-console.print("[code][bold yellow]Establecer banner", justify="center")
-banner = input()
-if banner == "":
-    banner = "Stellar"
-
-console.print("[code][bold yellow]Establecer fuente para el banner de texto, deje en blanco si no coloco un banner de texto.", justify="center")
-banner_font = input()
-if banner_font == "":
-    banner_font = "standard"
-
-console.print("[code][bold yellow]Establezca el texto personalizado de la input", justify="center")
-input_text = input()
-if input_text == "":
-    input_text = "Stellar"
-
-os.system(f"""
+try:
+    console.print("[code][bold yellow]Establecer banner", justify="center")
+    banner = input()
+    if banner == "":
+        banner = "Stellar"
+    
+    console.print("[code][bold yellow]Establecer fuente para el banner de texto, deje en blanco si no coloco un banner de texto.", justify="center")
+    banner_font = input()
+    if banner_font == "":
+        banner_font = "standard"
+        
+    console.print("[code][bold yellow]Establezca el texto personalizado de la input", justify="center")
+    input_text = input()
+    if input_text == "":
+        input_text = "Stellar"
+    
+    os.system(f"""
 cd
 cd .configs_stellar
 cd themes
@@ -30,4 +31,7 @@ echo {input_text} > input.txt
 cd
 """)
 
-console.print("[code][bold green]Configuración realizada con éxito, escriba bash para que los cambios surtan efecto", justify="center")
+    console.print("[code][bold green]Configuración realizada con éxito, escriba bash para que los cambios surtan efecto", justify="center")
+except Exception as e:
+    print(f"[code][bold red]Error: {e}")
+time.sleep(2)
