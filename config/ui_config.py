04e40a85abd6325c1a4297037236ca1bbd031150
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-01-21 00:31:39 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -10,20 +10,23 @@ try:
     
     main = console.input("[bold green]Elija el tipo de banner a agregar,[/bold green] [bold yellow][code]Ascii Art/Texto: ")
 
+    if main == "Ascii Art":
+        os.system("nano .configs_stellar/themes/banner.txt")
 
 
-    console.print("[bold yellow]Establecer banner")
-    banner = console.input("[bold green]> ")
-    if banner == "":
-        banner = "Stellar"
-    console.print("")
+    if main == "Texto":
+        console.print("[bold yellow]Establecer banner")
+        banner = console.input("[bold green]> ")
+        if banner == "":
+            banner = "Stellar"
+        console.print("")
 
-
-    console.print("[bold yellow]Establecer fuente para el banner de texto.")
-    banner_font = console.input("[bold green]> ")
-    if banner_font == "":
-        banner_font = "standard"
-    console.print("")
+    if main == "Texto":
+        console.print("[bold yellow]Establecer fuente para el banner de texto.")
+        banner_font = console.input("[bold green]> ")
+        if banner_font == "":
+            banner_font = "standard"
+        console.print("")
 
 
     console.print("[bold yellow]Establezca el texto personalizado de la input")
@@ -35,6 +38,7 @@ try:
 
     console.print("[code][bold green]Configuración realizada con éxito, escriba bash para que los cambios surtan efecto", justify="center")
     console.print()
+
 except Exception as e:
     print(f"[code][bold red]Error: {e}")
 time.sleep(2)
