cbade5a92557d89cf7d6044da284568fd7631714
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-02-28 23:12:11 -0400

    Actualizar traductor.py

--- misc/tools/traductor.py
+++ misc/tools/traductor.py
@@ -1,1 +1,16 @@
+import requests
+from rich.console import Console
+from rich.table import Table
 
+console = Console()
+
+API_KEY = "Kastg_fKlIk2c1LRc8969in2g9_free"
+
+https://api.kastg.xyz/api/tool/translate?input=Hello%20World!%20This%20is%20the%20output%20in%20french&to=fr&from=auto
+
+table = Table(title="Traductor", title_justify="center", title_style="bold magenta")
+table.add_column("", style="bold white", no_wrap=False)
+
+table.add_row("")
+
+console.print(table)
