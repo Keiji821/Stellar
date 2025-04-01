25826423893918c1ce3eb68565475e48257031cd
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-01 04:28:37 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -12,20 +12,24 @@ try:
 
     if main == "":
         os.system("cd && nano .configs_stellar/themes/banner.txt")
+
         color_menu = console.input("[bold green]Pulse [code][bold yellow]Enter[/bold yellow][/code] para ver colores disponibles: [bold green]")
+
         console.print("""[bold green]
 > bold green = verde brilloso
 > bold red = rojo brilloso
 > bold cyan = cyan brilloso
 > bold magenta = magenta brilloso
 > bold yellow = anaranjado brilloso
-[bold green]""")        
+[bold green]""")
+
         color = console.input("[bold green]Elija un color para su banner: [bold green]")
         os.system(f"""
 cd 
 cd .configs_stellar/themes
 echo {color} > banner_color.txt
 cd""")
+
         input_text = console.input("[bold green]Ingrese un texto para la input: [bold green]")
         os.system(f"""
 cd 
