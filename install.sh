ba3683ab4b01c7c6bc475e47ceba7ae371e9d46b
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-06 13:47:37 -0400

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -160,7 +160,25 @@ cd
 EOF
 
 
-echo Stellar > banner.txt
+cat > banner.txt << 'EOF'
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--'
+EOF
+
 echo bold green > banner_color.txt
 echo No > banner_background.txt
 echo Stellar > input.txt
