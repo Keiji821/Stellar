cba5947e0cc25b2e469991b44002cd0ceabf32a5
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-17 02:00:38 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -12,7 +12,7 @@ menu_data = {
         ("bash", "Reinicia la sesión"),
         ("ui", "Personalizar temas e interfaz")
         ("uninstall", "Desinstala todo el sistema"),
-        ("update", "Actualiza y busca actualizaciones en el repositorio de github."),
+        ("update", "Actualiza y busca actualizaciones en el repositorio de github.")
     ],
     "UTILIDADES": [
         ("ia", "Asistente IA con API integrada"),
