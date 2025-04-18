5a3e47587c32b14c4c213d41d3d2f489592da176
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-18 14:40:01 -0400

    Actualizar searchserverinvites.py

--- osint/discord/searchserverinvites.py
+++ osint/discord/searchserverinvites.py
@@ -1,1 +1,11 @@
- 
+import requests
+from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.table import Table
+from rich.prompt import Prompt
+from rich.panel import Panel
+from rich import box
+import re
+
+console = Console()
+
