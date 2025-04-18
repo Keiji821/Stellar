1452d1566a9f64afc9f2861183b70e10d897a8a7
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-18 15:15:20 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -31,7 +31,7 @@ menu_data = {
     "DISCORD": [
         ("userinfo", "Obtiene información sobre un ID"),
         ("serverinfo", "Obtiene información sobre un servidor"),
-        ("searchserverinvite", "Obtiene enlaces de invitación por palabras claves de fuentes públicas"),
+        ("searchserverinvites", "Obtiene enlaces de invitación por palabras claves de fuentes públicas"),
         ("inviteinfo", "Obtiene información sobre un enlace de invitación"),
     ],
     "ATAJOS": [
