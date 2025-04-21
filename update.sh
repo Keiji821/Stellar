8cb8ee161fb4721262f8bb0787c21cd046fbe991
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-20 23:44:06 -0400

    Actualizar update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+cd config/themes
+BANNER=(cat "banner.txt")
+BANNER_COLOR=(cat "banner_color.txt")
+BANNER_BACKGROUND=(cat "banner_background.txt")
+BANNER_BACKGROUND_COLOR=(cat "banner_background_color.txt")
+cd
+cd Stellar
+
 update_repo() {
     if ! git pull --force; then
         echo "Error al actualizar el repositorio" >&2
@@ -20,3 +28,12 @@ else
     show_lolcat_message "Falló la actualización" 5
     exit 1
 fi
+
+cd config/themes
+echo $BANNER > banner.txt
+echo $BANNER_COLOR > banner_color.txt
+echo $BANNER_BACKGROUND > banner_background.txt
+echo $BANNER_BACKGROUND_COLOR > banner_background_color.txt
+printf "¡Todo en orden!" | lolcat -a -d 20
+echo
+
