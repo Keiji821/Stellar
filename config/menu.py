c428a3359e795a5f89f1594b67c3da886d293755
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 18:44:13 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -15,12 +15,15 @@ def page1():
     console.print("[code][bold green]Comandos de Stellar[/code][/bold green]", justify="center")
     console.print("[bold white]Pagina principal[/bold white]", justify="center")
     
+def page2():
+    console.print("[code][bold cyan]OSINT[/code][/bold cyan]")
 
 while True:
     char = sys.stdin.read(1)
     if char == "1":
         page1()
-    elif char == "2":        
+    elif char == "2":
+        page2()
     elif char == "3":        
     elif char == "4":       
     elif char == "5":       
