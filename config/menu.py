f04d816c003044ca6d3eac2397d62d0190051652
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 18:53:33 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -11,23 +11,23 @@ fd = sys.stdin.fileno()
 attr = termios.tcgetattr(fd)
 termios.tcsetattr(fd, termios.TCSANOW, attr)
 
-def page1():
+def mainpage():
     console.print("[code][bold green]Comandos de Stellar[/code][/bold green]", justify="center")
     console.print("[bold white]Pagina principal[/bold white]", justify="center")
     
-def page2():
+def page1():
     console.print("[code][bold cyan]Sistema[/code][/bold cyan]")
 
-def page3():
+def page2():
     console.print("[code][bold cyan]Utilidades[/code][/bold cyan]")
 
-def page4():
+def page3():
     console.print("[code][bold cyan]Osint[/code][/bold cyan]")
 
-def page5():
+def page4():
     console.print("[code][bold cyan]Osint - Discod[/code][/bold cyan]")
 
-def page6():
+def page5():
     console.print("[code][bold cyan]Pentesting[/code][/bold cyan]")
 
 
@@ -42,10 +42,11 @@ while True:
     elif char == "4":  
         page4()     
     elif char == "5":  
-        page5()     
-    elif char == "6":  
-        page6()     
+        page5()        
     elif char == "x":
         break
 
 termios.tcsetattr(fd, termios.TCSANOW, attr)
+
+if __name__ == "__main__":
+    mainpage()
