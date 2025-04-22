03fb030a601c7e914367cdbe05ffa0ed150ed2e8
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 18:34:27 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -1,6 +1,9 @@
 from rich.console import Console
+from rich.markdown import Markdown
+from os import system
 import termios
 import sys
+import os
 
 console = Console()
 
@@ -8,12 +11,28 @@ fd = sys.stdin.fileno()
 attr = termios.tcgetattr(fd)
 termios.tcsetattr(fd, termios.TCSANOW, attr)
 
+def page1():
+    console.print("[code][bold green]Comandos de Stellar[/code][/bold green]", justify="center")
+    console.print("[bold white]Pagina principal[/bold white]", justify="center")
+    
+
 while True:
     char = sys.stdin.read(1)
-    if char == "w":
-        print("Has presionado la tecla w")
-    elif char == "s":
-        print("Has presionado la tecla s")
+    if char == "1":
+        
+
+    elif char == "2":
+        
+
+    elif char == "3":
+        
+
+    elif char == "4":
+        
+
+    elif char == "5":
+        
+
     elif char == "":
         break
 
