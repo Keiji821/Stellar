75844b9c5d1e91cf002eda13ae7d0596e1b0f3f7
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 20:14:17 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -7,22 +7,28 @@ console.print("""
 • ui > Personaliza el banner y sus colores 
 • uninstall > Desinstala Stellar
 • update > Actualiza desde el repositorio de github
-• bash > Reinicia su sesión de la terminal
+• bash > Reinicia su sesión de la terminal""")
 
+
+console.print("""
 • ia > Un servicio de ai desde de una API gratuita 
 • ia-image > Generador de imágenes IA
 • traductor > Traducción en tiempo real
-• myip > Muestra tu ip real
-   
-• ipinfo > Obtiene información de una ip 
+• myip > Muestra tu ip real""")
+
+
+console.print("""• ipinfo > Obtiene información de una ip 
 • urlinfo > Analizador de URL
 • userfinder > Busca un nombre de usuario en diferentes páginas 
 • phoneinfo > Obtiene información de un número de teléfono 
-• emailsearch > Búsqueda de emails
+• emailsearch > Búsqueda de emails""")
 
-• userinfo > Obtiene información apartir de una id
+
+console.print("""• userinfo > Obtiene información apartir de una id
 • serverinfo > Obtiene información sobre un servidor a partir de su id
 • searchinvites > Busca invitaciones en páginas ingresando el nombre del servidor
-• inviteinfo > Obtiene información sobre un enlace de invitación
+• inviteinfo > Obtiene información sobre un enlace de invitación""")
 
+
+console.print("""
 • ddos > Realiza un ataque ddos mediante la ip y puerto""")
