df266aff9a00144059df9a547b809df928a4c6e9
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 21:52:51 -0400

    Actualizar profileinfo.py

--- commands/osint/instagram/profileinfo.py
+++ commands/osint/instagram/profileinfo.py
@@ -1,1 +1,4 @@
-
+from instaloader import Instaloader, Profile
+   L = Instaloader()
+   profile = Profile.from_username(L.context, "usuario")
+   print(f"Bio: {profile.biography} | Seguidores: {profile.followers}")
