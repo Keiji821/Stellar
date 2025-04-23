5c05843dcef13525d8b6a3d03fd6890caec85273
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 20:22:32 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -8,28 +8,28 @@ console.print("""
 • ui > Personaliza el banner y sus colores 
 • uninstall > Desinstala Stellar
 • update > Actualiza desde el repositorio de github
-• bash > Reinicia su sesión de la terminal""")
+• bash > Reinicia su sesión de la terminal""", style="bold cyan")
 
-
+console.print("[code]Utilidades[code]", style="bold green", justify="center")
 console.print("""
 • ia > Un servicio de ai desde de una API gratuita 
 • ia-image > Generador de imágenes IA
 • traductor > Traducción en tiempo real
-• myip > Muestra tu ip real""")
+• myip > Muestra tu ip real""", style="bold cyan")
 
-
+console.print("[code]Osint[code]", style="bold green", justify="center")
 console.print("""• ipinfo > Obtiene información de una ip 
 • urlinfo > Analizador de URL
 • userfinder > Busca un nombre de usuario en diferentes páginas 
 • phoneinfo > Obtiene información de un número de teléfono 
-• emailsearch > Búsqueda de emails""")
+• emailsearch > Búsqueda de emails""", style="bold cyan")
 
-
+console.print("[code]Osint/Discord[code]", style="bold green", justify="center")
 console.print("""• userinfo > Obtiene información apartir de una id
 • serverinfo > Obtiene información sobre un servidor a partir de su id
 • searchinvites > Busca invitaciones en páginas ingresando el nombre del servidor
-• inviteinfo > Obtiene información sobre un enlace de invitación""")
+• inviteinfo > Obtiene información sobre un enlace de invitación""", style="bold cyan")
 
-
+console.print("[code]Pentesting[code]", style="bold green", justify="center")
 console.print("""
-• ddos > Realiza un ataque ddos mediante la ip y puerto""")
+• ddos > Realiza un ataque ddos mediante la ip y puerto""", style="bold cyan")
