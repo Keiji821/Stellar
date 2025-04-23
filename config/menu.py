c910a0b6bb7866b355d8c17c0b37130af7a52b10
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 20:23:27 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -10,6 +10,7 @@ console.print("""
 • update > Actualiza desde el repositorio de github
 • bash > Reinicia su sesión de la terminal""", style="bold cyan")
 
+console.print("")
 console.print("[code]Utilidades[code]", style="bold green", justify="center")
 console.print("""
 • ia > Un servicio de ai desde de una API gratuita 
@@ -17,6 +18,7 @@ console.print("""
 • traductor > Traducción en tiempo real
 • myip > Muestra tu ip real""", style="bold cyan")
 
+console.print("")
 console.print("[code]Osint[code]", style="bold green", justify="center")
 console.print("""• ipinfo > Obtiene información de una ip 
 • urlinfo > Analizador de URL
@@ -24,12 +26,14 @@ console.print("""• ipinfo > Obtiene información de una ip
 • phoneinfo > Obtiene información de un número de teléfono 
 • emailsearch > Búsqueda de emails""", style="bold cyan")
 
+console.print("")
 console.print("[code]Osint/Discord[code]", style="bold green", justify="center")
 console.print("""• userinfo > Obtiene información apartir de una id
 • serverinfo > Obtiene información sobre un servidor a partir de su id
 • searchinvites > Busca invitaciones en páginas ingresando el nombre del servidor
 • inviteinfo > Obtiene información sobre un enlace de invitación""", style="bold cyan")
 
+console.print("")
 console.print("[code]Pentesting[code]", style="bold green", justify="center")
 console.print("""
 • ddos > Realiza un ataque ddos mediante la ip y puerto""", style="bold cyan")
