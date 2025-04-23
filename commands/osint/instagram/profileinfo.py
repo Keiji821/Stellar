ebf7c846bf770620a9ad51523a5beb99943b6791
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 22:10:18 -0400

    Actualizar profileinfo.py

--- commands/osint/instagram/profileinfo.py
+++ commands/osint/instagram/profileinfo.py
@@ -7,6 +7,6 @@ usuario = console.input("[bold green]Ingrese un nombre de usuario: [bold green]"
 
 profile = Profile.from_username(L.context, usuario)
 
-console.print("[bold green]Usuario: [/bold green]", usuario
-console.print("[bold green]Bio: [/bold green]", {profile.biography} 
+console.print("[bold green]Usuario: [/bold green]", usuario)
+console.print("[bold green]Bio: [/bold green]", {profile.biography})
 console.print("[bold green]Seguidores: [/bold green]", {profile.followers})
