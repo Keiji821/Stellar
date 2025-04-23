d0267f62e8e56fab6aa72a6c5be4ba35257d9102
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-22 21:57:56 -0400

    Actualizar profileinfo.py

--- commands/osint/instagram/profileinfo.py
+++ commands/osint/instagram/profileinfo.py
@@ -1,4 +1,12 @@
 from instaloader import Instaloader, Profile
-   L = Instaloader()
-   profile = Profile.from_username(L.context, "usuario")
-   print(f"Bio: {profile.biography} | Seguidores: {profile.followers}")
+from rich.console import console
+
+L = Instaloader()
+
+usuario = console.input("[bold green]Ingrese un nombre de usuario: [bold green]")
+
+profile = Profile.from_username(L.context, usuario)
+
+console.print("[bold green]Usuario: [/bold green]", usuario
+console.print("[bold green]Bio: [/bold green]", {profile.biography} 
+console.print("[bold green]Seguidores: [/bold green]", {profile.followers})
