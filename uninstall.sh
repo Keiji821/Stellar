e66fff0f1fcc95b965af4ba0b1243b11c141932c
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-04-27 04:46:58 -0400

    Actualizar uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,58 +1,35 @@
-#!/bin/bash
+gris="${b}[1;30m"
+blanco="[0m"
+blanco2="$b[1;37m"
+rojo="${b}[1;31m"
+rojo2="${b}[31m"
+azul="${b}[1;34m"
+azul2="${b}[34m"
+azul_agua="${b}\e[1;36m"
+azul_agua2="${b}\e[36m"
+verde="${b}[1;32m"
+verde2="${b}[32m"
+morado="$b[1;35m"
+morado2="$b[35m"
+amarillo="$b[1;33m"
+amarillo2="$b[33m"
+cyan="$b[38;2;23;147;209m"
 
-gris="\033[1;30m"
-blanco="\033[0m"
-blanco2="\033[1;37m"
-rojo="\033[1;31m"
-rojo2="\033[31m"
-azul="\033[1;34m"
-azul2="\033[34m"
-azul_agua="\033[1;36m"
-azul_agua2="\033[36m"
-verde="\033[1;32m"
-verde2="\033[32m"
-morado="\033[1;35m"
-morado2="\033[35m"
-amarillo="\033[1;33m"
-amarillo2="\033[33m"
-cyan="\033[38;2;23;147;209m"
+read -p "${rojo}¿Desea borrar Stellar completamente? S/N${blanco} " respuesta
 
-respuesta=$(dialog --colors --backtitle "Stellar" \
-                  --title "Confirmación" \
-                  --clear \
-                  --yesno "${rojo}¿Desea borrar Stellar completamente? (S/N)${blanco}" 10 60)
+if [ "$respuesta" = "s" ] || [ "$respuesta" = "S" ] || [ "$respuesta" = "sí" ] || [ "$respuesta" = "Si" ]; then
+rm -rf Stellar && rm -rf .bashrc && rm -rf .bash_profile
 
-case $respuesta in
-    Yes)
-        rm -rf Stellar && rm -rf .bashrc && rm -rf .bash_profile
-        dialog --colors --backtitle "Stellar" \
-               --title "Éxito" \
-               --clear \
-               --msgbox "${verde}Stellar borrado con éxito${blanco}" 10 60
-        ;;
-    No|*)
-        dialog --colors --backtitle "Stellar" \
-               --title "Cancelado" \
-               --clear \
-               --msgbox "${rojo}Operación cancelada${blanco}" 10 60
-        ;;
-esac
+printf "${verde}Stellar borrado con éxito${blanco}"
+else
+printf "${rojo}Operación cancelada${blanco}"
+fi
 
-dialog --colors --backtitle "Stellar" \
-       --title "Cerrando sesión" \
-       --clear \
-       --infobox "Cerrando sesión....." 10 60
+printf "Cerrando sesión....." | lolcat -a -d 30
 
 clear
 
-dialog --colors --backtitle "Stellar" \
-       --title "Despedida" \
-       --clear \
-       --msgbox "Espero hayas disfrutado de mi herramienta! :3 Atte: Keiji821" 15 60
+printf "Espero hayas disfrutado de mi herramienta! :3 Atte: Keiji821" | lolcat -a -d 100
+printf "¡Adiós! :)" | lolcat -a -d 20
 
-dialog --colors --backtitle "Stellar" \
-       --title "Despedida" \
-       --clear \
-       --msgbox "¡Adiós! :)" 10 60
-
-exit 0
+login
