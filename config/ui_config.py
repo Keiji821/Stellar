c3601277a65d28f9244b1e7a2ad99f7de5155f61
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-01 18:05:27 -0400

    Actualizar ui_config.py

--- config/ui_config.py
+++ config/ui_config.py
@@ -5,3 +5,14 @@ from rich.table import Table
 import os
 from os import system
 
+console = Console()
+
+
+os.system("clear")
+
+
+def configbanner():
+    banner = console.input("¿Desea configurar el contenido del banner? (Sí/No): ", style="bold green")
+    if banner=="Sí":
+        os.system("rm -rf banner.txt")
+        os.system("cd && cd Stellar/config/themes && nano banner.txt")
