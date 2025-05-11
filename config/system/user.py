4e7fb61568b767f0c22913a16ccc05f34a81106e
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-11 17:48:54 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -8,7 +8,7 @@ console = Console()
 os.path.expanduser("~/Stellar/config/themes")
 
 user = open("user.txt", encoding="utf-8") as f:
-    return f.read().strip()
+           return f.read().strip()
 
 table = Table(title="Lista", title_justify="center", title_style="bold green")
 table.add_column(f"[bold green] Información", style="bold white", no_wrap=False)
