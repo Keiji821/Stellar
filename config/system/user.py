0a8e02372f9db070eee2a2958887ca886ca75f9d
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-11 18:02:35 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -11,13 +11,13 @@ with open("user.txt", encoding="utf-8") as f:
 os.path.expanduser("~/Stellar/config/themes")
 
 with open("banner_color.txt", encoding="utf-8") as f:
-    user = f.read().strip()
+    banner_color = f.read().strip()
 
 with open("banner_background.txt", encoding="utf-8") as f:
-    user = f.read().strip()
+    banner_background = f.read().strip()
 
 with open("banner_background_color.txt", encoding="utf-8") as f:
-    user = f.read().strip()
+    banner_background_color = f.read().strip()
 
 
 table = Table(title="Perfil", title_justify="center", title_style="bold green")
@@ -27,6 +27,9 @@ table.add_column("[bold green]Descripción", style="bold white")
 table.add_row("[code]General[/code]")
 table.add_row("Usuario", user)
 table.add_row("[code]Preferencias[/code]")
+table.add_row("Color del banner", banner_color)
+table.add_row("Fondo del banner", banner_background)
+table.add_row("Color del fondo del banner", banner_background_color)
 
 console.print(table, style="bright_cyan", justify="center")
 console.print("")
