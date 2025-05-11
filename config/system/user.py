b101e683b4cfd90d09115b1b3059cd0e0ba56274
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-11 17:53:24 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -15,7 +15,7 @@ table = Table(title="Lista", title_justify="center", title_style="bold green")
 table.add_column(f"[bold green] Información", style="bold white", no_wrap=False)
 table.add_column("[bold green]Descripción", style="bold white")
 
-table.add.row("Usuario", user)
+table.add_row("Usuario", user)
 
 console.print(table, style="bright_cyan", justify="center")
 console.print("")
