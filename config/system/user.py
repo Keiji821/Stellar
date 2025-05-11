a939a59a9de2fb4dfe25056de82ce7d62805ccb3
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-11 17:42:41 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -5,12 +5,16 @@ from rich.table import Table
 
 console = Console()
 
-os.chdir("~/Stellar/config/themes")
+os.path.expanduser("~/Stellar/config/themes")
+
+user = with open("user.txt", encoding="utf-8") as f:
+    return f.read().strip()
 
 table = Table(title="Lista", title_justify="center", title_style="bold green")
 table.add_column(f"[bold green] Información", style="bold white", no_wrap=False)
 table.add_column("[bold green]Descripción", style="bold white")
 
+table.add.row("Usuario", user)
 
 console.print(table, style="bright_cyan", justify="center")
 console.print("")
