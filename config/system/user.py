d90ef43641d55952f7608e3e392b43719c08b871
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-11 17:36:53 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -1,3 +1,17 @@
 from rich.console import Console
 import os
 from os import system
+from rich.table import Table
+
+console = Console()
+
+
+
+table = Table(title="Lista", title_justify="center", title_style="bold green")
+table.add_column(f"[bold green] Información", style="bold white", no_wrap=False)
+table.add_column("[bold green]Descripción", style="bold white")
+
+
+console.print(table, style="bright_cyan", justify="center")
+console.print("")
+
