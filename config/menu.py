84076eee8781b48002d4e4481ddb04a87110f4fb
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-05-29 17:50:24 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -23,48 +23,47 @@ table.add_column("[bold green]Descripción", style="bold white")
 table.add_column("[bold green]Estado", style="bold white")
 
 table.add_row("Sistema", style="bold green")
-
-table.add_row("• reload", "Recargar el banner")
-table.add_row("• ui", "Personaliza el banner y sus colores") 
-table.add_row("• uninstall", "Desinstala Stellar")
-table.add_row("• update", "Actualiza desde el repositorio de github")
-table.add_row("• bash", "Reinicia su sesión de la terminal")
+table.add_row("• reload", "Recargar el banner", "🟢 Activo")
+table.add_row("• ui", "Personaliza el banner y sus colores", "🟢 Activo") 
+table.add_row("• uninstall", "Desinstala Stellar", "🟢 Activo")
+table.add_row("• update", "Actualiza desde el repositorio de github", "🟢 Activo")
+table.add_row("• bash", "Reinicia su sesión de la terminal", "🟢 Activo")
 
 table.add_row("")
 table.add_row("Utilidades", style="bold green")
 
 table.add_row("• ia", "Un servicio de ai desde de una API gratuita", "🔴 En mantenimiento")
 table.add_row("• ia-image", "Generador de imágenes IA", "🔴 En mantenimiento")
-table.add_row("• traductor", "Traducción en tiempo real")
-table.add_row("• myip", "Muestra tu ip real")
+table.add_row("• traductor", "Traducción en tiempo real", "🟢 Activo")
+table.add_row("• myip", "Muestra tu ip real", "🟢 Activo")
 
 table.add_row("")
 table.add_row("Osint", style="bold green")
 
-table.add_row("• ipinfo", "Obtiene información de una ip") 
-table.add_row("• urlinfo", "Analizador de URL")
-table.add_row("• userfinder", "Busca un nombre de usuario en diferentes páginas")
-table.add_row("• phoneinfo", "Obtiene información de un número de teléfono")
-table.add_row("• metadatainfo", "Extrae metadatos de imágenes y documentos")
-table.add_row("• emailsearch", "Búsqueda de emails")
+table.add_row("• ipinfo", "Obtiene información de una ip", "🟢 Activo") 
+table.add_row("• urlinfo", "Analizador de URL", "🟢 Activo")
+table.add_row("• userfinder", "Busca un nombre de usuario en diferentes páginas", "🟢 Activo")
+table.add_row("• phoneinfo", "Obtiene información de un número de teléfono", "🟢 Activo")
+table.add_row("• metadatainfo", "Extrae metadatos de imágenes y documentos", "🟢 Activo")
+table.add_row("• emailsearch", "Búsqueda de emails", "🟢 Activo")
 
 table.add_row("")
 table.add_row("Osint/Discord", style="bold green")
 
-table.add_row("• userinfo", "Obtiene información apartir de una id")
-table.add_row("• serverinfo", "Obtiene información sobre un servidor a partir de su id")
-table.add_row("• searchinvites", "Busca invitaciones en páginas ingresando el nombre del servidor")
-table.add_row("• inviteinfo", "Obtiene información sobre un enlace de invitación")
+table.add_row("• userinfo", "Obtiene información apartir de una id", "🟢 Activo")
+table.add_row("• serverinfo", "Obtiene información sobre un servidor a partir de su id", "🟢 Activo")
+table.add_row("• searchinvites", "Busca invitaciones en páginas ingresando el nombre del servidor", "🟢 Activo")
+table.add_row("• inviteinfo", "Obtiene información sobre un enlace de invitación", "🟢 Activo")
 
 table.add_row("")
 table.add_row("Osint/Instagram", style="bold green")
 
-table.add_row("• profileinfo", "Obtiene los metadatos del perfil")
+table.add_row("• profileinfo", "Obtiene los metadatos del perfil", "🟢 Activo")
 
 table.add_row("")
 table.add_row("Pentesting", style="bold green")
 
-table.add_row("• ddos", "Realiza un ataque ddos mediante la ip y puerto")
+table.add_row("• ddos", "Realiza un ataque ddos mediante la ip y puerto", "🟢 Activo")
 
 
 console.print(table, style="bright_cyan", justify="center")
