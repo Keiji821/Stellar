b49280daef073c563ffafc8c17504e5b5b1a7774
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-06-24 14:38:05 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -5,7 +5,7 @@ import pyfiglet
 console = Console()
 
 console.print("")
-banner = pyfiglet.figlet_format("Comandos", font="standard")
+banner = pyfiglet.figlet_format("Comandos", font="poison")
 console.print(banner, style="bright_blue", justify="center")
 
 console.print("")
