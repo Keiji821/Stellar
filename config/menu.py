7053c176a1845da2d098aeb2c3535faa53b6165e
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-06-23 21:29:28 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -76,5 +76,5 @@ table.add_row("[code]Pentesting", style="bold green")
 table.add_row("")
 table.add_row("• ddos", "Realiza un ataque DDOS mediante la IP y puerto", "🟢 Activo")
 
-console.print(table, style="grey85", justify="center")
+console.print(table, style="orange1", justify="center")
 console.print("")
