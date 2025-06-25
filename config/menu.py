09fa307ae6357bd62be0b2ad3ab5a519e887322e
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-06-25 01:33:09 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -24,8 +24,7 @@ table.add_row("• update", "Actualiza desde el repositorio de github", "🟢 Ac
 table.add_row("• bash", "Reinicia su sesión de la terminal", "🟢 Activo")
 table.add_row("• history -c", "Elimina cache de comandos en la terminal", "🟢 Activo")
 table.add_row("• reset", "Reestablece la terminal", "🟢 Activo")
-table.add_row("• userconf", "Configura tu usuario de Stellar", "🟢 Activo")
-table.add_row("• my", "Visualiza tu perfil de Stellar", "🟢 Activo")
+table.add_row("• my", "Administra y configura tu perfil de Stellar", "🟢 Activo")
 
 table.add_row("")
 table.add_row("[code]Utilidades", style="bold green")
