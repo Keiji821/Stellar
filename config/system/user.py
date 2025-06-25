76078043780f6465b99581731013a48e85498186
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-06-25 02:10:51 -0400

    Actualizar user.py

--- config/system/user.py
+++ config/system/user.py
@@ -33,14 +33,15 @@ with open("banner_background_color.txt", encoding="utf-8") as f:
 
 # Usuario 
 
-if user=="":
-    while True:
-        response_user = console.input("No tiene un usuario configurado\n¿Desea configurarlo? (y/n): ", style="bold green")
-        if response_user=="y":
-            response_user_config = console.input("Ingrese un nombre de usuario: ", style="bold green")
-            if not response_user_config:
-                console.print("Error: No se puede dejar en blanco. Intente de nuevo.", style="bold red")
-                continue
+if user=="Stellar":
+    if user=="":
+        while True:
+            response_user = console.input("No tiene un usuario configurado\n¿Desea configurarlo? (y/n): ", style="bold green")
+            if response_user=="y":
+                response_user_config = console.input("Ingrese un nombre de usuario: ", style="bold green")
+                if not response_user_config:
+                    console.print("Error: No se puede dejar en blanco. Intente de nuevo.", style="bold red")
+                    continue
 
 
 table = Table(title="Perfil", title_justify="center", title_style="bold green")
