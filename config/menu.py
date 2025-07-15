768ad130feff84be7b4c4a43cef6989c175b0050
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-07-15 01:38:27 -0400

    Actualizar menu.py

--- config/menu.py
+++ config/menu.py
@@ -1,12 +1,7 @@
 from rich.console import Console
 from rich.table import Table
-import pyfiglet
 
 console = Console()
-
-console.print("")
-banner = pyfiglet.figlet_format("Comandos", font="ansi_shadow")
-console.print(banner, style="bright_magenta", justify="center")
 
 console.print("")
 
