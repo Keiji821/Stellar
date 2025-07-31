2bf84e04d22bf0f3d174987b98a55978b040887a
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-07-31 05:07:10 -0400

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ printf "${Azul_Brillante} $banner ${Reset}"
 printf "\n"
 printf "\n${Magenta_Brillante}⭐ Created by: Keiji821 ${Reset}"
 printf "\n${Cian_Brillante}🔵 My Discord contact: ${Verde_Brillante}keiji100 ${Reset}"
-printf "\n${Magenta_Brillante}📦 Repository ${Rojo_Brillante} >>> ${Cian_Brillante}${Subrayado}https://github.com/Keiji821/Stellar ${Reset}\n"
+printf "\n${Magenta_Brillante}📦 Repository ${Rojo_Brillante} >>> ${Cian_Brillante}${Subrayado}https://github.com/Keiji821/Stellar${Reset}\n"
 printf "\n"
 
 # Inicio/Start - Stellar
