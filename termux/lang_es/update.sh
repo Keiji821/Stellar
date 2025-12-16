7a4db7164408a89efeb770e6604a09ce60306d1c
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-12-16 01:49:44 -0400

    Actualizar update.sh

--- termux/lang_es/update.sh
+++ termux/lang_es/update.sh
@@ -37,13 +37,13 @@ safe_move() {
 backup_config() {
     show_progress "Resguardando archivos de configuración"
 
-    safe_move "$CONFIG_SYSTEM_DIR/user.txt" "$HOME" || return 1
-    safe_move "$CONFIG_SYSTEM_DIR/login_method.txt" "$HOME" || return 1
-    safe_move "$CONFIG_SYSTEM_DIR/password.txt" "$HOME" || return 1
-    safe_move "$CONFIG_THEMES_DIR/banner.txt" "$HOME" || return 1
-    safe_move "$CONFIG_THEMES_DIR/banner_color.txt" "$HOME" || return 1
-    safe_move "$CONFIG_THEMES_DIR/banner_background.txt" "$HOME" || return 1
-    safe_move "$CONFIG_THEMES_DIR/banner_background_color.txt" "$HOME" || return 1
+    safe_move "$CONFIG_SYSTEM_DIR/user.st" "$HOME" || return 1
+    safe_move "$CONFIG_SYSTEM_DIR/login_method.st" "$HOME" || return 1
+    safe_move "$CONFIG_SYSTEM_DIR/password.st" "$HOME" || return 1
+    safe_move "$CONFIG_THEMES_DIR/banner.st" "$HOME" || return 1
+    safe_move "$CONFIG_THEMES_DIR/banner_color.st" "$HOME" || return 1
+    safe_move "$CONFIG_THEMES_DIR/banner_background.st" "$HOME" || return 1
+    safe_move "$CONFIG_THEMES_DIR/banner_background_color.st" "$HOME" || return 1
 
     return 0
 }
@@ -71,13 +71,13 @@ update_repository() {
 restore_config() {
     show_progress "Restaurando configuraciones"
 
-    safe_move "$HOME/user.txt" "$CONFIG_SYSTEM_DIR/" || return 1
-    safe_move "$HOME/login_method.txt" "$CONFIG_SYSTEM_DIR/" || return 1
-    safe_move "$HOME/password.txt" "$CONFIG_SYSTEM_DIR/" || return 1
-    safe_move "$HOME/banner.txt" "$CONFIG_THEMES_DIR/" || return 1
-    safe_move "$HOME/banner_color.txt" "$CONFIG_THEMES_DIR/" || return 1
-    safe_move "$HOME/banner_background.txt" "$CONFIG_THEMES_DIR/" || return 1
-    safe_move "$HOME/banner_background_color.txt" "$CONFIG_THEMES_DIR/" || return 1
+    safe_move "$HOME/user.st" "$CONFIG_SYSTEM_DIR/" || return 1
+    safe_move "$HOME/login_method.st" "$CONFIG_SYSTEM_DIR/" || return 1
+    safe_move "$HOME/password.st" "$CONFIG_SYSTEM_DIR/" || return 1
+    safe_move "$HOME/banner.st" "$CONFIG_THEMES_DIR/" || return 1
+    safe_move "$HOME/banner_color.st" "$CONFIG_THEMES_DIR/" || return 1
+    safe_move "$HOME/banner_background.st" "$CONFIG_THEMES_DIR/" || return 1
+    safe_move "$HOME/banner_background_color.st" "$CONFIG_THEMES_DIR/" || return 1
 
     return 0
 }
