afa8d62df7511fa3c99bb944d1e1a410525ca909
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-12-21 01:25:20 -0400

    Actualizar banner.py

--- linux/lang_es/config/themes/banner.py
+++ linux/lang_es/config/themes/banner.py
@@ -77,13 +77,13 @@ def main():
                 elif is_proxy == True:
                     message_ip = "[bold green][!] [bold white]IP De Proxy"
                 if is_tor or is_vpn or is_proxy == False:
-                    message_ip = "[bold yellow][!] [bold white](IP Pública)"
+                    message_ip = "[bold yellow][!] [bold white]IP Pública"
                 else:
-                    message_ip = "[bold red][!] [bold white]IP No identificada)"
+                    message_ip = "[bold red][!] [bold white]IP No identificada"
                 return message_ip
             except Exception as e:
                 console.print(f"[bold red][STELLAR] [bold white]Ha ocurrido un error en Stellar, error: [bold red]{e}")
-                message_ip = "[bold red] Error al identificar IP"
+                message_ip = "[bold red][!] [bold white] Error al identificar IP"
                 return message_ip
         ip = http()
         message_ip = get_type_ip()
