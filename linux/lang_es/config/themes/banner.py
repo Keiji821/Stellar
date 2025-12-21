2da8f1502e0594ed35f59ab71602636778c54a79
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-12-21 13:11:24 -0400

    Actualizar banner.py

--- linux/lang_es/config/themes/banner.py
+++ linux/lang_es/config/themes/banner.py
@@ -156,7 +156,7 @@ def main():
                 table.add_row(f"{icon_disk} Disco:", disk_bar)
                 table.add_row("", f"{disk.used//(1024**3):,} GB / {disk.total//(1024**3):,} GB")
                 table.add_row(f"{icon_ip} IP", str(ip), message_ip)
-                table.add_row(f"{icon_palette} Paleta/Colores", f"{colors1}▅▅ {colors2}▅▅  {banner_color}▅▅ {banner_background_color}▅▅") 
+                table.add_row(f"{icon_palette} Paleta/Colores", f"[{colors1}]▅▅ [{colors2}]▅▅  [{banner_color}]▅▅ [{banner_background_color}]▅▅") 
                 panel = Panel(table, title="Sistema", border_style=f"{colors1}")
                 console.print(panel)
             except Exception as e:
