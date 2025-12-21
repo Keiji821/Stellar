53f842bdacdbde922601f45a935a8340b73ec493
Keiji <127261198+Keiji821@users.noreply.github.com>
2025-12-21 01:05:24 -0400

    Actualizar banner.py

--- linux/lang_es/config/themes/banner.py
+++ linux/lang_es/config/themes/banner.py
@@ -64,31 +64,35 @@ def main():
         banner_background_color = f.read().strip()
     try:
         def get_type_ip():
-            response = requests.get(f"https://api.ipapi.is/?ip=")
-            data = response.json()
-            is_tor = str(data.get("is_tor"))
-            if is_tor == True:
-                message_ip = "[bold green](IP De ToR)"
-            elif is_tor == False:
-                message_ip = "[bold yellow](IP Pública)"
-            else:
-                message_ip = "[bold red](IP No identificada)"
-            is_vpn = str(data.get("is_vpn"))
-            if is_vpn == True:
-                message_ip = "[bold green](IP Enmascarada)"
-            elif is_vpn == False:
-                message_ip = "[bold yellow](IP Pública)"
-            else:
-                message_ip = "[bold red](IP No identificada)"
-            is_proxy = str(data.get("is_proxy"))
-            if is_proxy == True:
-                message_ip = "[bold green](IP Enmascarada)"
-            elif is_proxy == False:
-                message_ip = "[bold yellow](IP Pública)"
-            else:
-                message_ip = "[bold red](IP No identificada)"
-            return message_ip
-        
+            try:
+                response = requests.get(f"https://api.ipapi.is/?ip=")
+                data = response.json()
+                is_tor = str(data.get("is_tor"))
+                if is_tor == True:
+                    message_ip = "[bold green](IP De ToR)"
+                elif is_tor == False:
+                    message_ip = "[bold yellow](IP Pública)"
+                else:
+                    message_ip = "[bold red](IP No identificada)"
+                is_vpn = str(data.get("is_vpn"))
+                if is_vpn == True:
+                    message_ip = "[bold green](IP Enmascarada)"
+                elif is_vpn == False:
+                    message_ip = "[bold yellow](IP Pública)"
+                else:
+                    message_ip = "[bold red](IP No identificada)"
+                is_proxy = str(data.get("is_proxy"))
+                if is_proxy == True:
+                    message_ip = "[bold green](IP Enmascarada)"
+                elif is_proxy == False:
+                    message_ip = "[bold yellow](IP Pública)"
+                else:
+                    message_ip = "[bold red](IP No identificada)"
+                return message_ip
+            except Exception as e:
+                console.print(f"[bold red][STELLAR] [bold white]Ha ocurrido un error en Stellar, error: [bold red]{e}")
+                message_ip "[bold red] Error al identificar IP"
+                return message_ip
         ip = http()
         message_ip = get_type_ip()
         console.print(banner, style=f"{banner_color}")
